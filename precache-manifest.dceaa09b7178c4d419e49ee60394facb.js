02efc83c8b3d9c3f6930bd80a498ae201c8880bb
Corey Short <corey.short@serenaandlily.com>
2025-04-03 14:50:56 -0700

    Updates

---
+++ precache-manifest.dceaa09b7178c4d419e49ee60394facb.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d28f2651df416dd1ee3732bc195afc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78a9c6ab37ce139b1d5b",
+    "url": "./static/css/2.cc5a7cc5.chunk.css"
+  },
+  {
+    "revision": "f4ec8796a88a6c793c26",
+    "url": "./static/css/main.660799a2.chunk.css"
+  },
+  {
+    "revision": "78a9c6ab37ce139b1d5b",
+    "url": "./static/js/2.a8cb4149.chunk.js"
+  },
+  {
+    "revision": "0e9cc79d5f3eaa1d9515875a04deeafe",
+    "url": "./static/js/2.a8cb4149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ec8796a88a6c793c26",
+    "url": "./static/js/main.9f13aab9.chunk.js"
+  },
+  {
+    "revision": "bfb875d3a6590b7b2856",
+    "url": "./static/js/runtime-main.22a1bba6.js"
+  },
+  {
+    "revision": "28878623fbda2eb1ecc013e84c57c423",
+    "url": "./static/media/ibm-plex-sans-cyrillic-400-normal.28878623.woff2"
+  },
+  {
+    "revision": "bc251380f7854113b3154999e0897d99",
+    "url": "./static/media/ibm-plex-sans-cyrillic-400-normal.bc251380.woff"
+  },
+  {
+    "revision": "00b082966f425b1eea3cbd7cacfb2d2c",
+    "url": "./static/media/ibm-plex-sans-cyrillic-500-normal.00b08296.woff2"
+  },
+  {
+    "revision": "3560f168b3154e19e6d506bd8486e311",
+    "url": "./static/media/ibm-plex-sans-cyrillic-500-normal.3560f168.woff"
+  },
+  {
+    "revision": "a6b932e088f5dd8f7547cdf75d4f45bb",
+    "url": "./static/media/ibm-plex-sans-cyrillic-ext-400-normal.a6b932e0.woff2"
+  },
+  {
+    "revision": "df6d1a175ff8f93e43dae64ade335290",
+    "url": "./static/media/ibm-plex-sans-cyrillic-ext-400-normal.df6d1a17.woff"
+  },
+  {
+    "revision": "18beaa988dd6807706dbc73be92cf08a",
+    "url": "./static/media/ibm-plex-sans-cyrillic-ext-500-normal.18beaa98.woff2"
+  },
+  {
+    "revision": "b9ac2bafee6991b3072c309401b3abbd",
+    "url": "./static/media/ibm-plex-sans-cyrillic-ext-500-normal.b9ac2baf.woff"
+  },
+  {
+    "revision": "a2dc9c19db3c6c32cc744dc3712be2d5",
+    "url": "./static/media/ibm-plex-sans-greek-400-normal.a2dc9c19.woff"
+  },
+  {
+    "revision": "a802921a1f014c3694bdb5d56e7e4e1f",
+    "url": "./static/media/ibm-plex-sans-greek-400-normal.a802921a.woff2"
+  },
+  {
+    "revision": "032c507e83094966af7790fdface354c",
+    "url": "./static/media/ibm-plex-sans-greek-500-normal.032c507e.woff2"
+  },
+  {
+    "revision": "217b31e2a4d3d79a7acf87e71a125b3b",
+    "url": "./static/media/ibm-plex-sans-greek-500-normal.217b31e2.woff"
+  },
+  {
+    "revision": "0ceb759015a6df090ad355231fdb39f1",
+    "url": "./static/media/ibm-plex-sans-latin-400-normal.0ceb7590.woff2"
+  },
+  {
+    "revision": "9217623a253873caf3a5f9821a26ee64",
+    "url": "./static/media/ibm-plex-sans-latin-400-normal.9217623a.woff"
+  },
+  {
+    "revision": "767677e475131fa7d3f37880976bee39",
+    "url": "./static/media/ibm-plex-sans-latin-500-normal.767677e4.woff2"
+  },
+  {
+    "revision": "7bad45ee47144170440d8aea2959e670",
+    "url": "./static/media/ibm-plex-sans-latin-500-normal.7bad45ee.woff"
+  },
+  {
+    "revision": "0e2c71ca88ef614ed360af7147277927",
+    "url": "./static/media/ibm-plex-sans-latin-ext-400-normal.0e2c71ca.woff2"
+  },
+  {
+    "revision": "40eaad2f83dd3f427315f35fbcc201c0",
+    "url": "./static/media/ibm-plex-sans-latin-ext-400-normal.40eaad2f.woff"
+  },
+  {
+    "revision": "9e50dcc81c7e8dbfeb68273080276154",
+    "url": "./static/media/ibm-plex-sans-latin-ext-500-normal.9e50dcc8.woff2"
+  },
+  {
+    "revision": "cc496ebb30186e00e1cb02ff9df0cd2b",
+    "url": "./static/media/ibm-plex-sans-latin-ext-500-normal.cc496ebb.woff"
+  },
+  {
+    "revision": "8269cd1caa07b3f38189523c3c47c99d",
+    "url": "./static/media/ibm-plex-sans-vietnamese-400-normal.8269cd1c.woff2"
+  },
+  {
+    "revision": "82b296357552e22d26c012ff41204de7",
+    "url": "./static/media/ibm-plex-sans-vietnamese-400-normal.82b29635.woff"
+  },
+  {
+    "revision": "1e00ae58c0c3ffe53b4d9e9cbd0c8119",
+    "url": "./static/media/ibm-plex-sans-vietnamese-500-normal.1e00ae58.woff"
+  },
+  {
+    "revision": "e5351342329340d3fa35590c53d098d4",
+    "url": "./static/media/ibm-plex-sans-vietnamese-500-normal.e5351342.woff2"
+  },
+  {
+    "revision": "e895996c16df20b14470b7b2cb9d9b27",
+    "url": "./static/media/info-icon.e895996c.svg"
+  }
+]);
